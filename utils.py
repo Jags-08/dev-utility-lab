1b974437e30e2ffada87581a38d36b1cbfb0a109
Jags-08 <jagsjoshi75@gmail.com>
2026-05-03 20:33:10 +0530

    Add is_prime function

--- utils.py
+++ utils.py
@@ -37,3 +37,22 @@ def is_even(n):
     Returns True if n is an even number, False otherwise.
     """
     return n % 2 == 0
+
+def is_prime(n):
+    """
+    Checks if a number is prime.
+    Returns True if prime, False otherwise.
+    """
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
