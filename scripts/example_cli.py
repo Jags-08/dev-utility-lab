0ee6ea70a0046bdc0ed367a501cc6a2acebada47
Jagrut Joshi <jagsjoshi75@gmail.com>
2026-05-12 13:45:20 +0530

    docs: add minimal argparse template to examples (#15)

---
+++ scripts/example_cli.py
@@ -0,0 +1,10 @@
+﻿import argparse
+def main():
+    """Example CLI Tool"""
+    parser = argparse.ArgumentParser(description="Lab Utility")
+    parser.add_argument("--verbose", action="store_true")
+    args = parser.parse_args()
+    if args.verbose:
+        print("Verbose mode on")
+if __name__ == "__main__":
+    main()
